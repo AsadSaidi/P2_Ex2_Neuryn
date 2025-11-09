d0f34d827146c1769b9890dee6f3180aa6081b37
AsadSaidi <asad.saidi.7e8@itb.cat>
2025-11-09 17:47:41 +0100

    Footer añadido

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="flex items-center justify-between px-6 py-4 bg-slate-900/40 backdrop-blur-sm border-t border-slate-700/50 shadow-xl">
+      <div className="flex items-center space-x-12">
+        <nav>
+          <ul className="flex space-x-8 text-white text-base font-normal">
+            <li className="hover:text-slate-300 cursor-pointer">
+              <a href="mailto:info@neuryn.com">Contacto</a>
+            </li>
+            <li className="hover:text-slate-300 cursor-pointer">
+              <a href="#">Términos y Condiciones</a>
+            </li>
+            <li className="hover:text-slate-300 cursor-pointer">
+              <a href="#">Política de Privacidad</a>
+            </li>
+            <li className="hover:text-slate-300 cursor-pointer">
+              <a href="#">Sobre Nosotros</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      
+    <div className="text-white text-sm">
+        &copy; {new Date().getFullYear()} Neuryn
+    </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,25 @@
 import { Header } from "../components/Header";
 import PostsSection from "../components/PostSections";
-
 import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 
  const Home = () => {
   return (
-    <div className="bg-gradient-to-br from-slate-950 via-blue-950 to-slate-900 min-h-screen">
-  <Header />
-  <div className="max-w-7xl mx-auto px-6 py-6 grid grid-cols-[1fr_480px] gap-6">
-    {/* Posts Section */}
-    <div className="h-screen overflow-y-auto pr-4 scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-transparent">
-      <PostsSection />
-    </div>
+    <div className="bg-gradient-to-br from-slate-950 via-blue-950 to-slate-900 min-h-screen relative">
+      <Header />
+      <div className="max-w-7xl mx-auto px-6 py-8 grid grid-cols-[1fr_480px] gap-6">
+        {/* Posts Section */}
+        <div className="calc-h-[100vh-theme(spacing.32)] overflow-y-auto pr-4 scrollbar-thin scrollbar-thumb-slate-700 scrollbar-track-transparent">
+          <PostsSection />
+        </div>
 
-    {/* Sidebar */}
-    <div className="hidden lg:block h-screen">
-      <Sidebar />
+        {/* Sidebar */}
+        <div className="hidden lg:block calc-h-[100vh-theme(spacing.32)]">
+          <Sidebar />
+        </div>
+      </div>
+      <Footer />
     </div>
-  </div>
-</div>
 
   );
 };
